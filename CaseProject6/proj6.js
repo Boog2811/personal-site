e379f37ede5fb126fe116a50c5b52b55fe7a2a28
James Emanuel <jemanuel2811@gmail.com>
2025-03-17 14:09:04 -0400

    feat: username validation, removal of password validation
    i realized that i put the username rules in and implemented it for password, this commit remedies that

--- CaseProject6/proj6.js
+++ CaseProject6/proj6.js
@@ -7,20 +7,22 @@ function validateSignUp(event) {
     let numbersCheck = /\d/; //checks if the string contains a number
     let specialCharactersCheck = /[!@#$%^&*(),.?":{}|<>\/\\]/; //checks if the string contains a special character
 
-    //Password
-    //Password must be between 6 and 15 characters long
+    //Username
+    let initialNumbersCheck = /^\d/; //checks if the string begins with a number
+
+    //Username
+    //Username must be between 6 and 15 characters long
     //only contain letters and numbers (no special characters)
     //and cannot start with a number.
-    let password = document.forms["signUpForm"].elements["password"].value;
-    let passwordError = "Your password ";
-    let initialNumbersCheck = /^\d/; //checks if the string begins with a number
-    let errorMessages = [];
+    let username = document.forms["signUpForm"].elements["username"].value;
+    let usernameErrorStart = "Your username ";
+    let usernameErrorMessages = [];
     try {
-        if (password.length < 8) errorMessages.push("is too short"); //checking for less than 6 characters
-        if (password.length > 20) errorMessages.push("is too long"); //checking for more than 15 characters
-        if (specialCharactersCheck.test(password)) errorMessages.push("has a special character"); //checking for special characters using regex
-        if (initialNumbersCheck.test(password)) errorMessages.push("starts with a number"); //checking if the password starts with a number through .charCodeAt & UTF-16 character codes
-        if (errorMessages.length > 0) throw passwordError + errorMessages.join(" + ") + ".";
+        if (username.length < 6) usernameErrorMessages.push("is too short"); //checking for less than 6 characters
+        if (username.length > 15) usernameErrorMessages.push("is too long"); //checking for more than 15 characters
+        if (specialCharactersCheck.test(username)) usernameErrorMessages.push("has a special character"); //checking for special characters using regex
+        if (initialNumbersCheck.test(username)) usernameErrorMessages.push("starts with a number"); //checking if the username starts with a number using regex
+        if (errorMessages.length > 0) throw usernameErrorStart + usernameErrorMessages.join(" + ") + ".";
     }
     catch (error) {
         console.log(error);

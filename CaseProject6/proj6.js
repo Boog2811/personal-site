8248a23e2a0c421585c9cdedb54eb880dbba1d9f
James Emanuel <jemanuel2811@gmail.com>
2025-03-17 17:22:13 -0400

    feat: full name validation, fixed username validation

--- CaseProject6/proj6.js
+++ CaseProject6/proj6.js
@@ -10,7 +10,28 @@ function validateSignUp(event) {
     //Username
     let initialNumbersCheck = /^\d/; //checks if the string begins with a number
 
-    //Username
+    //Email
+    let emailCheck = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/; //checks if the string is an email (not fully compliant with RFC 5322)
+
+    //  
+
+
+    //Full Name Validation
+    //Full name must be a non-empty string (no numbers or special characters).
+    let fullName = document.forms["signUpForm"].elements["fullName"].value;
+    let fullNameErrorStart = "Your full name ";
+    let fullNameErrorMessages = [];
+    try {
+        if (fullName.trim() == "") fullNameErrorMessages.push("is empty"); //checking for non-empty string
+        if (numbersCheck.test(fullName)) fullNameErrorMessages.push("contains a number"); //checking for a number
+        if (specialCharactersCheck.test(fullName)) fullNameErrorMessages.push("contains a special character"); //checking for special characters
+        if (fullNameErrorMessages.length > 0) throw fullNameErrorStart + fullNameErrorMessages.join(" + ") + ".";
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+    //Username Validation
     //Username must be between 6 and 15 characters long
     //only contain letters and numbers (no special characters)
     //and cannot start with a number.
@@ -18,15 +39,15 @@ function validateSignUp(event) {
     let usernameErrorStart = "Your username ";
     let usernameErrorMessages = [];
     try {
+        if (username.trim() == "") usernameErrorMessages.push("is empty"); //checking for non-empty string
         if (username.length < 6) usernameErrorMessages.push("is too short"); //checking for less than 6 characters
         if (username.length > 15) usernameErrorMessages.push("is too long"); //checking for more than 15 characters
-        if (specialCharactersCheck.test(username)) usernameErrorMessages.push("has a special character"); //checking for special characters using regex
-        if (initialNumbersCheck.test(username)) usernameErrorMessages.push("starts with a number"); //checking if the username starts with a number using regex
-        if (errorMessages.length > 0) throw usernameErrorStart + usernameErrorMessages.join(" + ") + ".";
+        if (specialCharactersCheck.test(username)) usernameErrorMessages.push("contains a special character"); //checking for special characters
+        if (initialNumbersCheck.test(username)) usernameErrorMessages.push("starts with a number"); //checking if the username starts with a number
+        if (usernameErrorMessages.length > 0) throw usernameErrorStart + usernameErrorMessages.join(" + ") + ".";
     }
     catch (error) {
         console.log(error);
-        return;
     }
 }
 
